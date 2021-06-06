eccbd19f07368a0b132f404993a86077bce636fe
Efremov Nikolay <efrem_nn@mail.ru>
2021-06-07 01:02:23 +0700

    Домашняя работа урока №6 доделал 3 задание

---
+++ lesson6/catalog/engine/db.php
@@ -0,0 +1,9 @@
+<?php
+
+function getDb() {
+    static $db = '';
+    if(empty($db)){
+        $db = @mysqli_connect('localhost', 'nikolay', '123456', 'shopphp') or die('Ошибка: ' . mysqli_connect_error());
+    }
+    return $db;
+}
